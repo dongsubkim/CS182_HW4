c15ed5583022aac6f19c0f28336ddd55ec0a97a8
dongsubkim <ds.kim@berkeley.edu>
2019-04-27 20:04:27 -0700

    dpn implemented

--- dqn.py
+++ dqn.py
@@ -297,10 +297,13 @@ class QLearner(object):
 
         self.replay_buffer_idx = self.replay_buffer.store_frame(self.last_obs)
         #recent_frame = self.replay_buffer.encode_recent_observation()
-        if not self.model_initialized or self.exploration.value(self.t) > np.random.uniform(size=1)[0]:
-            action = np.random.randint(0, self.num_actions-1)
+        #if not self.model_initialized or self.exploration.value(self.t) > np.random.uniform(size=1)[0]:
+        #    action = np.random.randint(0, self.num_actions-1)
         #else:
         #    action = self.session.run(self.action, feed_dict={self.obs_t_ph:[recent_frame]})[0]
+        random_num = random.random()
+        if not self.model_initialized or random_num < self.exploration.value(self.t):
+            action = random.randint(0, self.num_actions - 1)
         else:
             action = self.session.run(self.action,
                 feed_dict={self.obs_t_ph: 
